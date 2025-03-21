ea35111243d91cc1d61f3c11fa72d3db91f514c6
Muffiko <mimestoner0089@gmail.com>
2025-03-21 20:11:15 +0100

    example mvc structure

---
+++ repairmanagementsystem.server/RepairManagementSystem/Controllers/WeatherController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using RepairManagementSystem.Models;
+
+namespace RepairManagementSystem.Controllers;
+
+[ApiController]
+[Route("api/[controller]")] //[controller] will be replaced by the name of the controller class without the Controller suffix.
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    [HttpGet]
+    public IEnumerable<WeatherForecast> GetWeatherForecasts()
+    {
+        return Enumerable.Range(1, 5).Select(index =>
+            new WeatherForecast(
+                DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                Random.Shared.Next(-20, 55),
+                Summaries[Random.Shared.Next(Summaries.Length)]
+            )).ToArray();
+    }
+}
+
--- repairmanagementsystem.server/RepairManagementSystem/Program.cs
+++ repairmanagementsystem.server/RepairManagementSystem/Program.cs
@@ -1,8 +1,6 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.OpenApi.Models;
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCors(options =>
 {
@@ -14,40 +12,37 @@ builder.Services.AddCors(options =>
     });
 });
 
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1.0", new OpenApiInfo
+    {
+        Title = "RepairManagementSystemAPI",
+        Version = "v1.0",
+        Description = "ASP.NET Core Web API for RMSystem",
+        Contact = new OpenApiContact
+        {
+            Name = "TAB",
+            Email = "email@email.com",
+        }
+    });
+});
 var app = builder.Build();
 
 app.UseCors("AllowFrontend");
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+       {
+           options.SwaggerEndpoint("/swagger/v1.0/swagger.json", "RMS");
+       });
 }
 
+app.MapControllers();
 //app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching",
-};
-
-app.MapGet("/api/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
