6af040af34883247e0f6c9f1aebb7c7605ae4eee
Muffiko <mimestoner0089@gmail.com>
2025-03-25 21:44:51 +0100

    refactor: small stylistic changes

--- repairmanagementsystem.server/RepairManagementSystem/Controllers/WeatherController.cs
+++ repairmanagementsystem.server/RepairManagementSystem/Controllers/WeatherController.cs
@@ -1,6 +1,7 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using RepairManagementSystem.Models;
-using Microsoft.AspNetCore.Authorization;
+
 namespace RepairManagementSystem.Controllers;
 
 [ApiController]
@@ -9,19 +10,39 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing",
+        "Bracing",
+        "Chilly",
+        "Cool",
+        "Mild",
+        "Warm",
+        "Balmy",
+        "Hot",
+        "Sweltering",
+        "Scorching",
+        "Freezing",
+        "Bracing",
+        "Chilly",
+        "Cool",
+        "Mild",
+        "Warm",
+        "Balmy",
+        "Hot",
+        "Sweltering",
+        "Scorching",
     };
 
     [HttpGet]
     [Authorize]
     public IEnumerable<WeatherForecast> GetWeatherForecasts()
     {
-        return Enumerable.Range(1, 5).Select(index =>
-            new WeatherForecast(
+        return Enumerable
+            .Range(1, 5)
+            .Select(index => new WeatherForecast(
                 DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 Random.Shared.Next(-20, 55),
                 Summaries[Random.Shared.Next(Summaries.Length)]
-            )).ToArray();
+            ))
+            .ToArray();
     }
 }
-
