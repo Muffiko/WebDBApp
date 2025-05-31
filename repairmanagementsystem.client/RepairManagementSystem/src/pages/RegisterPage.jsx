a9e78564b8f15035459c7969676f6e26b674b735
Wonsski <radek.rajda@hotmail.com>
2025-05-31 14:35:42 +0200

    add JWT decoding and implement protected routes for authentication

--- repairmanagementsystem.client/RepairManagementSystem/src/pages/RegisterPage.jsx
+++ repairmanagementsystem.client/RepairManagementSystem/src/pages/RegisterPage.jsx
@@ -1,9 +1,17 @@
 // src/pages/RegisterPage.jsx
 import React from "react";
 import RegisterForm from "../components/RegisterForm";
+import { useAuth } from "../contexts/AuthContext";
+import { Navigate } from "react-router-dom";
 import "./styles/RegisterPage.css";
 
 const RegisterPage = () => {
+  const { isAuthenticated } = useAuth();
+
+  if (isAuthenticated) {
+    return <Navigate to="/requests" replace />;
+  }
+
   return (
     <div className="register-page">
       <RegisterForm />
