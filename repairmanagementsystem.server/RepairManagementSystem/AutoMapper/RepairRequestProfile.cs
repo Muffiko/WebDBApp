e760d1483531810e9854f80dbf4e51e1744d147c
muffiko <37244097+Muffiko@users.noreply.github.com>
2025-06-06 23:44:54 +0200

    feat: add customer-specific repair request retrieval and response mapping

--- repairmanagementsystem.server/RepairManagementSystem/AutoMapper/RepairRequestProfile.cs
+++ repairmanagementsystem.server/RepairManagementSystem/AutoMapper/RepairRequestProfile.cs
@@ -16,6 +16,11 @@ namespace RepairManagementSystem.AutoMapper
                 .ForMember(dest => dest.RepairObjectName, opt => opt.MapFrom(src => src.RepairObject.Name))
                 .ForMember(dest => dest.RepairObjectType, opt => opt.MapFrom(src => src.RepairObject.RepairObjectType));
             CreateMap<RepairRequestResponse, RepairRequest>();
+            CreateMap<RepairRequest, RepairRequestCustomerResponse>()
+                .ForMember(dest => dest.Request_Id, opt => opt.MapFrom(src => src.RepairRequestId))
+                .ForMember(dest => dest.ManagerName, opt => opt.MapFrom(src => src.ManagerId.HasValue
+                    ? src.Manager.User.FirstName + " " + src.Manager.User.LastName
+                    : null));
         }
     }
 }
