79791db6c07c31c0ad84b8980990e5ad18d50222
Michal <michal.kaminski123456789@gmail.com>
2025-06-19 14:56:44 +0200

    feat:PATCH method for activities works

--- repairmanagementsystem.client/RepairManagementSystem/src/components/ActivitiesList.jsx
+++ repairmanagementsystem.client/RepairManagementSystem/src/components/ActivitiesList.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import ActivityCard from "./ActivityCard";
 import "./styles/ActivitiesList.css";
 
-const ActivitiesList = ({ activities, onAdd }) => {
+const ActivitiesList = ({ activities, onAdd, onUpdate }) => {
+  const sortedActivities = [...activities].sort(
+    (a, b) => Number(b.sequenceNumber) - Number(a.sequenceNumber)
+  );
+
   return (
     <div className="activities-wrapper">
       <div className="activities-header">
@@ -13,19 +17,20 @@ const ActivitiesList = ({ activities, onAdd }) => {
       </div>
 
       <div className="activities-cards">
-        {activities.map(act => (
+        {sortedActivities.map(act => (
           <ActivityCard
             key={act.repairActivityId}
-            id={act.sequenceNumber}
+            id={act.repairActivityId}
             sequenceNumber={act.sequenceNumber}
             name={act.name}
             activityType={act.activityType}
             description={act.description}
-            worker={act.worker}
+            workerId={act.workerId}
             status={act.status}
             createdAt={act.createdAt}
             startedAt={act.startedAt}
             finishedAt={act.finishedAt}
+            onUpdate={onUpdate}
           />
         ))}
       </div>
